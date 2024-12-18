6399e6582500987776aa03c06f1d540853e8c853
RalphsCode <RalphsCode@gmail.com>
2024-12-18 11:32:44 -0800

    Initial Project set-up.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        RalphsCode - To-Do app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
